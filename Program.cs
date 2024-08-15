178e5f415f034faf27dff8761788145fe408be99
William Woodard <williamwoodard98@gmail.com>
2024-08-14 19:21:40 -0500

    User Interaction in the Console

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Thrown for a Loop!");
-Console.WriteLine("Please choose an option: ");
+﻿string greeting = @"Welcome to Thrown For a Loop
+Your one-stop shop for used sporting equipment";
+Console.WriteLine(greeting);
+Console.WriteLine("Please enter a product name: ");
+
+string response = Console.ReadLine();
+Console.WriteLine(@$"You chose: {response}. 
+Thank you for your input!");
