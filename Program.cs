e129fad4c2bdd128852a0c557b6f7c13c846ce73
William Woodard <williamwoodard98@gmail.com>
2024-08-16 14:04:08 -0500

    adding datetime

--- Program.cs
+++ Program.cs
@@ -7,37 +7,49 @@ List<Product> products = new List<Product>()
     { 
         Name = "Football", 
         Price = 15, 
-        Sold = false
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     },
     new Product() 
     { 
         Name = "Hockey Stick", 
         Price = 12, 
-        Sold = false
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     },
     new Product()
     {
         Name = "Cheese",
         Price = 4,
-        Sold = true
+        Sold = true,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     },
     new Product()
     {
         Name = "Coal",
         Price = 1,
-        Sold = false
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     },
     new Product()
     {
         Name = "Bat",
         Price = 18,
-        Sold = false
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     },
     new Product()
     {
         Name = "Baseball",
         Price = 10,
-        Sold = false
+        Sold = false,
+        StockDate = new DateTime(2022, 10, 20),
+        ManufactureYear = 2010
     }
 };
 
@@ -60,5 +72,11 @@ while (response > products.Count || response < 1)
     response = int.Parse(Console.ReadLine().Trim());
 }
 
+DateTime now = DateTime.Now;
+
 Product chosenProduct = products[response - 1];
-Console.WriteLine($"You chose: {chosenProduct.Name}, which costs {chosenProduct.Price} dollars and is {(chosenProduct.Sold ? "" : "not ")}sold.");
+TimeSpan timeInStock = now - chosenProduct.StockDate;
+Console.WriteLine(@$"You chose: 
+{chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
+It is {now.Year - chosenProduct.ManufactureYear} years old. 
+It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
